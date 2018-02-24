df51bf7973a10275c58d68f8eada4676b499c18f
Elias Faiçal <eliasfaical@gmail.com>
2018-02-24 14:38:24 -0300

    renomeando pasta

--- frontend-cubo/src/app/app.module.ts
+++ frontend-cubo/src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { UICarouselModule } from 'ui-carousel';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BannerHeroComponent } from './banner-hero/banner-hero.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BannerHeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UICarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
